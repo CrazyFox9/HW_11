4d9131e56e0573b2f58e9a8ee31e0843a53797fd
Eduard Kozhukhov <eduardbeliy@mail.ru>
2022-04-28 19:17:55 +0300

    Homework 11 complated

---
+++ app.py
@@ -0,0 +1,32 @@
+from flask import Flask, request, render_template
+
+import utils
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def main_page():
+    candidates = utils.load_candidates_from_json("candidates.json")
+    return render_template('list.html', candidates=candidates)
+
+
+@app.route('/candidate/<int:candidate_id>')
+def candidate_page(candidate_id):
+    candidates = utils.get_candidate(candidate_id)
+    return render_template('card.html', candidate=candidates)
+
+
+@app.route('/search/<string:candidate_name>')
+def search_page(candidate_name):
+    candidates = utils.get_candidates_by_name(candidate_name)
+    return render_template('search.html', candidates=candidates, candidates_count=len(candidates))
+
+
+@app.route('/skill/<string:skill_name>')
+def skill_page(skill_name):
+    candidates = utils.get_candidates_by_skill(skill_name)
+    return render_template('skills.html', candidates=candidates, skill=skill_name, candidates_count=len(candidates))
+
+
+app.run()
---
+++ utils.py
@@ -0,0 +1,41 @@
+import json
+
+
+def load_candidates_from_json(path):
+    """ возвращает список всех кандидатов """
+
+    with open(path, "r", encoding="UTF-8") as file:
+        candidates = json.load(file)
+        return candidates
+
+
+def get_candidate(candidate_id):
+    """ возвращает одного кандидата по его id """
+
+    candidates = load_candidates_from_json("candidates.json")
+    for candidate in candidates:
+        if candidate["id"] == candidate_id:
+            return candidate
+
+
+def get_candidates_by_name(candidate_name):
+    """ возвращает кандидатов по имени """
+
+    candidates = load_candidates_from_json("candidates.json")
+    result = []
+    for candidate in candidates:
+        if candidate_name.lower() in candidate["name"].lower():
+            result.append(candidate)
+    return result
+
+
+def get_candidates_by_skill(skill_name):
+    """ возвращает кандидатов по навыку """
+
+    candidates = load_candidates_from_json("candidates.json")
+    result = []
+    for candidate in candidates:
+        if skill_name.lower() in candidate["skills"].lower().split(", "):
+            result.append(candidate)
+    return result
+
